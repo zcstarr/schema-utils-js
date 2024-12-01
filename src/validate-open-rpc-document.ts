1f6dfefd76df7fc071133ee10e0b917a22d83b73
Zane Starr <zcstarr@gmail.com>
2024-11-30 16:38:10 -0800

    fix: add missing file

--- src/validate-open-rpc-document.ts
+++ src/validate-open-rpc-document.ts
@@ -1,7 +1,7 @@
 import metaSchema, { OpenrpcDocument as OpenRPC } from "@open-rpc/meta-schema";
 import Ajv, { ErrorObject } from "ajv";
 import JsonSchemaMetaSchema from "@json-schema-tools/meta-schema";
-import getMetaSchemaExtended from "./get-meta-schema-with-extension";
+import extensionSchema from "./open-rpc-extensions-schema.json"
 
 /**
  * @ignore
@@ -52,13 +52,20 @@ export default function validateOpenRPCDocument(
 ): OpenRPCDocumentValidationError | true {
   const ajv = new Ajv();
   ajv.addSchema(JsonSchemaMetaSchema, "https://meta.json-schema.tools");
-  //const metaSchemaCopy = { ...metaSchema } as any;
-  const metaSchemaCopy = getMetaSchemaExtended() as any;
-  // const metaSchemaCopy = { ...metaSchema } as any;
+  const metaSchemaCopy = { ...metaSchema } as any;
+  // const metaSchemaCopy = getMetaSchemaExtended() as any;
+  // const oldMetaSchemaCopy = { ...metaSchema } as any;
+  const extensionMetaSchemaCopy = { ...extensionSchema } as any;
+
+  // Process the extension and meta schema to remove the $id and $schema properties
+  delete extensionMetaSchemaCopy.$schema;
+  delete extensionMetaSchemaCopy.$id;
   delete metaSchemaCopy.definitions.JSONSchema.$id;
   delete metaSchemaCopy.definitions.JSONSchema.$schema;
   delete metaSchemaCopy.$schema;
   delete metaSchemaCopy.$id;
+  metaSchemaCopy.properties['x-extensions'] ={"$ref":"#/definitions/x-extensions"} 
+  metaSchemaCopy.definitions['x-extensions']=extensionMetaSchemaCopy.properties['x-extensions'];
   try {
     ajv.validate(metaSchemaCopy, document);
   } catch (e) {
@@ -70,8 +77,6 @@ export default function validateOpenRPCDocument(
       'If you see this report it: https://github.com/open-rpc/schema-utils-js/issues',
     ].join('\n'));
   }
-
-//  ajv.validate(metaSchemaCopy, document);
 
   if (ajv.errors) {
     console.log(document)
